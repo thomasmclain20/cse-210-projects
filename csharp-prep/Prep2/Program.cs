3c4c5006c795ba0e00accc691545979c5bf5c4ce
thomasmclain20 <tjmclain1@byui.edu>
2025-09-19 17:42:42 -0600

    Grade calculator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your percentage?");
+        string input = Console.ReadLine();
+        int percentage = int.Parse(input);
+    
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"You are a: {letter} student");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed, study hard and try again.");
+        }
+    
     }
 }
